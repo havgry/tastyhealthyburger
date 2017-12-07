d8e51fb953db32b07dd57e4251c9c5d3e8149334
Magnus Havgry <magnushavgry@gmail.com>
2017-12-07 23:38:27 +0100

    Create burger attributes and disable logic

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <toggle-button v-model="testValue"/>
-    <input type="checkbox" v-model="testValue"/>
-  </div>
+  <ul>
+    <li v-for="(attribute, index) in attributes" :key="attribute.id">
+      <toggle-button v-model="attribute.value" @input="disableAttribute(index)"/>
+      {{ attribute.label }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import _some from 'lodash/some'
+
 import ToggleButton from './ToggleButton.vue'
 
 export default {
@@ -14,8 +18,40 @@ export default {
     ToggleButton,
   },
   data: () => ({
-    testValue: true,
+    attributes: [{
+      id: 'tasty',
+      label: 'Tasty',
+      value: false,
+    }, {
+      id: 'healthy',
+      label: 'Healthy',
+      value: false,
+    }, {
+      id: 'burger',
+      label: 'Burger',
+      value: false,
+    }],
   }),
+  computed: {
+    areAnyAttributesDisabled() {
+      return _some(this.attributes, attribute => attribute.value === false)
+    },
+  },
+  methods: {
+    disableAttribute(toggledAttributeIndex) {
+      if (this.areAnyAttributesDisabled === false) {
+        const indexToDisable = this.getRandomInArray(this.attributes, toggledAttributeIndex)
+        this.attributes[indexToDisable].value = false
+      }
+    },
+    getRandomInArray(array, indexToExclude) {
+      const randomIndex = Math.floor(Math.random() * array.length)
+      if (randomIndex === indexToExclude) {
+        return this.getRandomInArray(array, indexToExclude)
+      }
+      return randomIndex
+    },
+  },
 }
 </script>
 
